8d038a13da83dd22046586c38dc074c6511f92dd
N. Martin Abregu <nmabregu@gmail.com>
2021-02-05 16:57:10 -0300

    cuando teacher inicia sesion lo mandamos a '/teacher' en otro caso a la raiz '/'

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -63,4 +63,12 @@ class LoginController extends Controller
             $this->username() => [trans('auth.failed')],
         ]);
     }
+
+    public function redirectPath()
+    {
+        if (auth()->user()->isTeacher()) {
+            return '/teacher';
+        }
+        return '/';
+    }
 }
