7c2e7f37612b69a1500634da9fcb21c8e35bd232
N. Martin Abregu <nmabregu@gmail.com>
2021-02-05 17:23:51 -0300

    creando traits para q los controladores hereden el comportamineto

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -2,24 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Course;
-use Illuminate\Http\Request;
+use App\Traits\Teacher\ManageCourses;
 
 class TeacherController extends Controller
 {
+    use ManageCourses;
+    
     public function index()
     {
         return view('teacher.index');
-    }
-
-    public function courses()
-    {
-        $courses = Course::forTeacher();
-        return view('teacher.courses.index', compact('courses'));
-    }
-
-    public function createCourse()
-    {
-        # code...
     }
 }
