087dabaa70deeeab9f0791c918840ecaf034a8d1
N. Martin Abregu <nmabregu@gmail.com>
2021-02-05 00:23:13 -0300

    comportamiento para cursos destacados

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,14 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
-use Illuminate\Http\Request;
+use App\Models\Course;
 
 class WelcomeController extends Controller
 {
     public function index()
     {
         $categories = Category::withCount('courses')->get();
+        $featuredCourses = Course::withCount('students')
+            ->with('categories', 'teacher')
+            ->whereFeatured(true)
+            ->whereStatus(Course::PUBLISHED)
+            ->get();
 
-        return view('welcome', compact('categories'));
+        return view('welcome', compact('categories', 'featuredCourses'));
     }
 }
